00c33a5158a6f08564dbd22eb238d0af1fa628ea
ken.freeman@gmail.com <MXHQE0BmxDxS>
2016-11-07 00:02:26 -0500

    Required fields, more columns, state lookup logic

--- rideshareCars.go
+++ rideshareCars.go
@@ -15,11 +15,11 @@ import (
 
 type RideshareCar struct {
 	Key string
-	state string
-	plateNumber	string
+	State string
+	PlateNumber	string
 	Make	string
 	Model string
-	color string
+	Color string
 }
 type RideshareCarView struct {
 	whenSeen    time.Time
@@ -30,8 +30,6 @@ type RideshareCarView struct {
 func init() {
 	http.HandleFunc("/", showRideShareCars)
 	http.HandleFunc("/cars", carsWebService)
-	http.HandleFunc("/showRideShareCars", showRideShareCars)
-	http.HandleFunc("/addCar", addCar)
 }
 
 func getCars(context appengine.Context) ([]RideshareCar){
@@ -122,6 +120,9 @@ func carFromForm(context appengine.Context, form url.Values, method string)(Ride
 	if (method != http.MethodDelete) {
 		car.Make = form.Get("Make")
 		car.Model = form.Get("Model")
+		car.State = form.Get("State")
+		car.PlateNumber = form.Get("PlateNumber")
+		car.Color = form.Get("Color")
 	}
 	return car, key
 }
@@ -133,28 +134,6 @@ func showRideShareCars(responseWriter http.ResponseWriter, _ *http.Request) {
 	}
 }
 
-func addCar(writer http.ResponseWriter, request *http.Request) {
-	context := appengine.NewContext(request);
-	// [END new_context]
-	car := RideshareCar{
-		Make: request.FormValue("make"),
-		Model: request.FormValue("model"),
-	}
-	context.Infof("car created. Make = " + car.Make)
-
-	// We set the same parent key on every Greeting entity to ensure each Greeting
-	// is in the same entity group. Queries across the single entity group
-	// will be consistent. However, the write rate to a single entity group
-	// should be limited to ~1/second.
-	key := datastore.NewIncompleteKey(context, "RideshareCar", nil)
-	_, err := datastore.Put(context, key, &car)
-	if err != nil {
-		http.Error(writer, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	http.Redirect(writer, request, "/showRideShareCars", http.StatusFound)
-	// [END if_user]
-}
 
 
 
